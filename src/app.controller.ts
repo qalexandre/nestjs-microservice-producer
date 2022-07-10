1afd941720dd97e5079ae3b0d2fdf01d4bccc73b
Alexandre Queiroz <alexandre.goncalves33@outlook.com>
2022-07-10 17:51:31 -0300

    create microservice

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,43 @@
 import { Controller, Get } from '@nestjs/common';
+import { Client, ClientKafka, Transport } from '@nestjs/microservices';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Client({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: ['localhost:9093'],
+      },
+      consumer: {
+        groupId: 'ec-consumer',
+      },
+    },
+  })
+  client: ClientKafka;
+
+  // Envia mensagem esperando resposta
+  async onModuleInit(){
+    this.client.subscribeToResponseOf('storage-ec');
+    await this.client.connect();
   }
+
+  @Get()
+  sendToStorage() {
+    return this.client.send('storage-ec', {
+      message: 'remove pizza from storage',
+    });
+  }
+
+
+  // Só envia a mensagem
+  // @Get()
+  // sendToStorage() {
+  //   return this.client.emit('storage-ec', {
+  //     message: 'remove ice cream from storage',
+  //   });
+  // }
 }
